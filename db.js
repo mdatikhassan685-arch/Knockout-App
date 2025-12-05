5fb220ac42a04b1a3d78cb292a125bd66b0cb0a2
mdatikhassan685-arch <mdatikhassan685@gmail.com>
2025-12-06 01:06:28 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,25 +1,18 @@
 const mysql = require('mysql2/promise');
 
-// Vercel-এ গ্লোবাল কানেকশন ক্যাশ করা
-let pool;
-
-if (!global.dbPool) {
-    global.dbPool = mysql.createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: 4000, // TiDB Port
-        ssl: {
-            minVersion: 'TLSv1.2',
-            rejectUnauthorized: true
-        },
-        waitForConnections: true,
-        connectionLimit: 1, // সার্ভারলেসের জন্য কম রাখাই ভালো
-        queueLimit: 0
-    });
-}
-
-pool = global.dbPool;
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 4000,
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 module.exports = pool;
