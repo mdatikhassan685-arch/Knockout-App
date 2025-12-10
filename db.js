9cc1a25f872b056cb8ef6705ffb3d05b29a66e88
mdatikhassan685-arch <mdatikhassan685@gmail.com>
2025-12-10 21:38:54 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,26 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
-// Vercel Environment থেকে তথ্য নেবে
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: 3306,
-    ssl: { rejectUnauthorized: true }
-});
+// গ্লোবাল কানেকশন পুল
+let pool;
 
-module.exports = pool.promise();
+if (!pool) {
+    pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT || 4000, // এখানে পরিবর্তন করা হয়েছে
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+        ssl: {
+            minVersion: 'TLSv1.2',
+            rejectUnauthorized: true
+        }
+    });
+}
+
+const db = pool.promise();
+
+module.exports = db;
